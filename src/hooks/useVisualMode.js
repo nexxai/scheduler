693f944e1713f2ac133e7dbd0a4fbf6a046cdfff
JT Smith <nexxai@gmail.com>
2023-10-10 14:11:15 -0600

    Adding ability to replace a transition point

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,12 +3,19 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) => {
-    setHistory((prev) => [...prev, newMode]);
+  const transition = (newMode, replace = false) => {
+    setHistory((prev) => {
+      if (replace) {
+        return [...prev.slice(0, prev.length - 1), newMode];
+      }
+      return [...prev, newMode];
+    });
   };
 
   const back = () => {
-    setHistory((prev) => [...prev.slice(0, prev.length - 1)]);
+    if (history[history.length - 1] !== "FIRST") {
+      setHistory((prev) => [...prev.slice(0, prev.length - 1)]);
+    }
   };
 
   return { mode: history[history.length - 1], transition, back };
