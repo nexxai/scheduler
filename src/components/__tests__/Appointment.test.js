5c86cdceb20fc2f6a8197884bb9be36b0324b02a
JT Smith <nexxai@gmail.com>
2023-10-11 15:55:59 -0600

    Replacing Application test with more useful one

--- src/components/__tests__/Appointment.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Application from "components/Application";
-
-describe("Application", function () {
-  it("renders without crashing", () => {
-    render(<Application />);
-  });
-});
